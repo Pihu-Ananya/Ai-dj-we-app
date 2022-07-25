e3336ca5b67024af66eda554a066506957c82781
Pihu-Ananya <97041050+Pihu-Ananya@users.noreply.github.com>
2022-07-25 16:42:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,6 +29,9 @@ function getposes(results) {
         console.log(leftwristX , leftwristY , rightwristX , rightwristY)
         leftwrist_score=results[0].pose.keypoints[9].score
         console.log(leftwrist_score)
+        rightwrist_score=results[0].pose.keypoints[10].score
+        console.log(rightwrist_score)
+
     }
 }
 
@@ -47,6 +50,31 @@ function draw() {
         volume=removedecimals/500
         song.setVolume(volume)
         document.getElementById("volume").innerHTML="volume:"+volume
+    }
+    if(rightwrist_score>0.2){
+        fill("green")
+        stroke("black")
+        circle(rightwristX , rightwristY , 30)
+        if(rightwristY>=0&&rightwristy<=100){
+            song.rate(0.5)
+            document.getElementById("speed").innerHTML="speed:0.5x"
+        }
+        if(rightwristY>=100&&rightwristy<=200){
+            song.rate(1)
+            document.getElementById("speed").innerHTML="speed:1x"
+        }
+        if(rightwristY>=200&&rightwristy<=300){
+            song.rate(1.5)
+            document.getElementById("speed").innerHTML="speed:1.5x"
+        }
+        if(rightwristY>=300&&rightwristy<=400){
+            song.rate(2)
+            document.getElementById("speed").innerHTML="speed:2x"
+        }
+        if(rightwristY>=400&&rightwristy<=500){
+            song.rate(2.5)
+            document.getElementById("speed").innerHTML="speed:2.5x"
+        }
     }
 }
 function play() {
